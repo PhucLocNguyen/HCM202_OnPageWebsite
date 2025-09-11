a582ae1bf043e3fb11607531028b8de4a884eb4f
PhucLocNguyen <loc19992003bca@gmail.com>
2025-09-11 21:55:54 +0700

    Fix context

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import RenderRoute from "./routes/RenderRoute";
-import { privateRoutes, publicRoutes } from "./routes/Route";
+import {  publicRoutes } from "./routes/Route";
 
 function App() {
   return (
       <Router>
         <Routes>
           {publicRoutes.map((route, index) => RenderRoute(route, index))}
-          {privateRoutes.map((route, index) => RenderRoute(route, index, true))}
         </Routes>
       </Router>
   );
--- src/routes/PrivateRouting.jsx
+++ src/routes/PrivateRouting.jsx
@@ -1,9 +1,6 @@
-import { useContext } from "react";
 import { Navigate, Outlet, useLocation } from "react-router";
-import { AuthContext } from "../context/AuthContext";
 
 function PrivateRouting() {
-  const { user } = useContext(AuthContext);
   const location = useLocation();
   if (user==null) {
     return <Navigate to="/login" state={{ from: location }} replace />;
